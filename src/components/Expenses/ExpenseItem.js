1ea8c9ed466b3305db872cbdc625744d96a6b853
Jesse Abarca <JesseAbarca888@gmail.com>
2023-01-29 22:50:39 -0800

    Event Handler example onClick

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,6 +4,11 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+
+  const clickHandler = () => {
+    console.log('clicked')
+  }
+
   return (
     <Card className="expense-item">
       <div className="expense-item__description">
@@ -11,6 +16,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
