606ab01baae23a0d4768cab4bb05d88259e7af95
Jesse Abarca <JesseAbarca888@gmail.com>
2023-01-30 17:36:21 -0800

    Create components ExpenseForm and NewExpense

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Expenses from "./components/Expenses/Expenses";
 import ExpenseItem from "./components/Expenses/ExpenseItem";
+import NewExpense from "./components/NewExpense/NewExpense";
 /*
 Assignment: Create a component that replaces ExpenseItem
 Expenses component that takes expenses as props
@@ -31,7 +32,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses expenses={expenses}/>
     </div>
   );
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
+
+function NewExpense() {
+    return (
+        <div className="new-expense">
+            <ExpenseForm />
+        </div>
+    );
+}
+
+export default NewExpense;
