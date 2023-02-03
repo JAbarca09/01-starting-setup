07fcebf853f1296aa90b7f7f9efa347ba07d0050
Jesse Abarca <JesseAbarca888@gmail.com>
2023-02-03 15:01:33 -0800

    Make ExpenseItem a stateless and remove btn

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
 
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    console.log("clicked");
-    setTitle("Updated Title");
-  };
-
   return (
     <Card className="expense-item">
       <div className="expense-item__description">
         <ExpenseDate date={props.date} />
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
