cc1744a0351c1d474bc19d0f5472a02559d94dde
Jesse Abarca <JesseAbarca888@gmail.com>
2023-02-03 22:12:52 -0800

    Filter works but only after a user selects a year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,23 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI/Card';
-import ExpensesFilter from './ExpensesFilter';
-import './Expenses.css';
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const [isFilterTrue, setIsFilteredTrue] = useState(false);
+  const [filterArr, setFilterArr] = useState([]);
 
-  const filterChangeHandler = selectedYear => {
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
+    setIsFilteredTrue(true);
+    const FilteredArr = props.items.filter((expense) => expense.date.getFullYear() === parseInt(selectedYear));
+    setFilterArr(FilteredArr);
   };
 
   return (
     <div>
-      <Card className='expenses'>
-        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {
-          props.items.map((element) => (
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {isFilterTrue == false
+          ? props.items.map((element) => (
+            <ExpenseItem
+              key={element.id}
+              title={element.title}
+              amount={element.amount}
+              date={element.date}
+            />
+          ))
+          :
+          filterArr.map((element) => (
             <ExpenseItem
               key={element.id}
               title={element.title}
