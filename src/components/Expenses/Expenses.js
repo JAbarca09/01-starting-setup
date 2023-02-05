2bc2527884779aac446bbd06cebacf37a9c6987d
Jesse Abarca <JesseAbarca888@gmail.com>
2023-02-04 23:53:13 -0800

    Add conditional rendering if there are no expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,8 +13,21 @@ const Expenses = (props) => {
   };
 
   const filteredExpenses = props.items.filter((expense) => {
-    return expense.date.getFullYear().toString() === filteredYear
+    return expense.date.getFullYear().toString() === filteredYear;
   })
+
+  let expensesContent = <p>No expenses found</p>;
+
+  if(filteredExpenses.length > 0){
+    expensesContent =  filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+  }
 
   return (
     <div>
@@ -23,16 +36,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {
-          filteredExpenses.map((expense) => (
-            <ExpenseItem 
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          ))
-        }
+        {expensesContent}
       </Card>
     </div>
   );
