589c70b6d0008743cb13271ae24fbd2d112f1c4c
Jesse Abarca <JesseAbarca888@gmail.com>
2023-02-13 17:59:57 -0800

    Add listeners and form handler to InputForm

--- 01-practice-project/src/Components/InputForm.js
+++ 01-practice-project/src/Components/InputForm.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./InputForm.module.css";
 
 const InputForm = () => {
+  const [userName, setUserName] = useState("");
+  const [userAge, setUserAge] = useState("");
+
+  const userNameHandler = (e) => {
+    setUserName(e.target.value);
+  };
+
+  const userAgeHandler = (e) => {
+    setUserAge(e.target.value);
+  };
+
+  const addUser = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className={styles.FormParentDiv}>
-      <form>
+      <form onSubmit={addUser}>
         <div className={styles.FormContentsDiv}>
           <h2 className={styles.smallerH2}>Username</h2>
-          <input className={styles.inputField} id={styles.firstInputField} type="text" />
+          <input
+            className={styles.inputField}
+            id={styles.firstInputField}
+            type="text"
+            onChange={userNameHandler}
+          />
           <h2 className={styles.smallerH2}>Age (Years)</h2>
-          <input className={styles.inputField} type="number" />
+          <input className={styles.inputField} type="number" onChange={userAgeHandler} />
+          <button type="submit">Add User</button>
         </div>
       </form>
     </div>
