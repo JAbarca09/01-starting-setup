781bcfe8995f080e79c04c094e371ac3e83c0b40
Jesse Abarca <JesseAbarca888@gmail.com>
2023-02-15 22:28:55 -0800

    Conditional  render if there are no users

--- 01-practice-project/src/Components/Users.js
+++ 01-practice-project/src/Components/Users.js
@@ -4,23 +4,34 @@ import styles from "./Users.module.css";
 const Users = (props) => {
 
     const usersArr = props.usersArr;
-
     //Go through the lists of users and render them!
+    //the component needs to be reevaluated using a useState
     return (
         <div className={styles.ParentDiv}>
-            <div className={styles.BackgroundForUsersComponent}>
-                <div className={styles.ParentUserPadding}>
-                    {
-                        usersArr.map(user => {
-                            return(
+            {usersArr.length > 0 ? (
+                <div className={styles.BackgroundForUsersComponent}>
+                    <div className={styles.ParentUserPadding}>
+                        {usersArr.map(user => {
+                            return (
                                 <p key={user.key} className={styles.user}>{user.Username} ({user.Age} Years Old)</p>
                             )
-                        })
-                    }
-                </div>
-            </div>
+                        })}
+                    </div>
+                </div>)
+                :
+                <h2 className={styles.noUsersH2}>There are no users...</h2>
+            }
         </div>
     );
 };
 
 export default Users;
+
+{/* usersArr.map(user => {
+            
+            return (
+            <div className={styles.ParentUserPadding}>
+                <p key={user.key} className={styles.user}>{user.Username} ({user.Age} Years Old)</p>
+            </div>
+            )
+        })) */}
