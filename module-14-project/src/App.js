1e87f8f01dc55023c28f75b7daaaa4394f13876c
Jesse Abarca <JesseAbarca888@gmail.com>
2023-03-18 01:09:24 -0700

    Refactor JSX to be cleaner

--- module-14-project/src/App.js
+++ module-14-project/src/App.js
@@ -18,11 +18,11 @@ function App() {
 
     try {
       const response = await fetch("https://swapi.dev/api/films/");
-      
+
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
-      
+
       const data = await response.json();
 
       const transformedMovies = data.results.map((movieData) => {
@@ -41,17 +41,26 @@ function App() {
     setIsLoading(false);
   };
 
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && !error && <p>Found no movies.</p>}
-        {!isLoading && error && <p>{error}</p>}
-        {isLoading && <p>Loading...</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
