b550ded56b97a5cbe88377f1272f2e04c7c06a2f
Jesse Abarca <JesseAbarca888@gmail.com>
2023-03-20 22:11:07 -0700

    Add Post req to Firebase backend for movies

--- module-14-project/src/App.js
+++ module-14-project/src/App.js
@@ -13,7 +13,7 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch("https://react-test-project-7e7be-default-rtdb.firebaseio.com/movies.json");
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
@@ -39,8 +39,16 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  async function addMovieHandler(movie) {
+    const response = await fetch("https://react-test-project-7e7be-default-rtdb.firebaseio.com/movies.json", {
+      method: "POST",
+      body: JSON.stringify(movie), 
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    const data = await response.json();
+    console.log(data);
   }
 
   let content = <p>Found no movies.</p>;
