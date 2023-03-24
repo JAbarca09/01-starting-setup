9278ed3485fbb99fcfd7fcfee8f12353a90fa33f
Jesse Abarca <JesseAbarca888@gmail.com>
2023-03-23 22:35:23 -0700

    Refactor Forward/Backward counter with useCounter

--- module-15-project/src/components/ForwardCounter.js
+++ module-15-project/src/components/ForwardCounter.js
@@ -1,8 +1,8 @@
-import Card from './Card';
+import Card from "./Card";
 import useCounter from "../hooks/use-content";
 
 const ForwardCounter = () => {
-  const counter = useCounter(1);
+  const counter = useCounter();
 
   return <Card>{counter}</Card>;
 };
