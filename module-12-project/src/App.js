8627c82116da237643e02dcca5a38f54b5a6db7b
Jesse Abarca <JesseAbarca888@gmail.com>
2023-04-03 00:10:29 -0700

    Add a button to enable toggling

--- module-12-project/src/App.js
+++ module-12-project/src/App.js
@@ -6,17 +6,25 @@ import "./App.css";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log("APP RUNNING");
 
   const toggleParagraphHandler = useCallback(() => {
-    setShowParagraph((showParagraph) => !showParagraph);
-  }, []);
+    if (allowToggle) {
+      setShowParagraph((showParagraph) => !showParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
       <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
