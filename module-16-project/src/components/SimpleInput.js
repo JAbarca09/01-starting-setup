8f8d8695be43695b30f079244a16a4f9c7b8fe6e
Jesse Abarca <JesseAbarca888@gmail.com>
2023-04-25 22:35:38 -0700

    Adding basic validation SimpleInput.js

--- module-16-project/src/components/SimpleInput.js
+++ module-16-project/src/components/SimpleInput.js
@@ -1,11 +1,32 @@
+import { useState, useRef } from 'react';
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
+  
+  const nameInputChangeHandler = event => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = event => {
+    event.preventDefault();
+
+    if (enteredName.trim() === '') {
+      return;
+    }
+
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} />
       </div>
-      <div className="form-actions">
+      <div className='form-actions'>
         <button>Submit</button>
       </div>
     </form>
