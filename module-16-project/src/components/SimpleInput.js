0a777226e9725ba35cd559527799fc357453522f
Jesse Abarca <JesseAbarca888@gmail.com>
2023-04-27 00:52:47 -0700

    Refactor enteredNameIsValid to false

--- module-16-project/src/components/SimpleInput.js
+++ module-16-project/src/components/SimpleInput.js
@@ -3,7 +3,8 @@ import { useState, useRef } from 'react';
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('');
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouch, setEnteredNameTouch] = useState(false);
   
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value);
@@ -24,14 +25,16 @@ const SimpleInput = (props) => {
     console.log(enteredValue);
   };
 
-  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid'
+  const nameInputIsValid = !enteredNameIsValid && enteredNameTouch;
+
+  const nameInputClasses = nameInputIsValid ? 'form-control invalid' : 'form-control';
 
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} />
-        {!enteredNameIsValid && <p className='error-text'>Name must not be empty!</p>}
+        {nameInputIsValid && <p className='error-text'>Name must not be empty!</p>}
       </div>
       <div className='form-actions'>
         <button>Submit</button>
