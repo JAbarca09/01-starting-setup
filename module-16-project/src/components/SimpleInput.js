00efb38a01fbdd21dc4a358dfd6ccd678f375f38
Jesse Abarca <JesseAbarca888@gmail.com>
2023-04-29 00:35:11 -0700

    Add onBlur input validation on SimpleInput

--- module-16-project/src/components/SimpleInput.js
+++ module-16-project/src/components/SimpleInput.js
@@ -10,6 +10,15 @@ const SimpleInput = (props) => {
     setEnteredName(event.target.value);
   };
 
+  const nameInputBlurHandler = event => {
+    setEnteredNameTouch(true);
+
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+      return;
+    };
+  };
+
   const formSubmissionHandler = event => {
     event.preventDefault();
 
@@ -35,7 +44,7 @@ const SimpleInput = (props) => {
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} />
+        <input ref={nameInputRef} type='text' id='name' onBlur={nameInputBlurHandler} onChange={nameInputChangeHandler} value={enteredName} />
         {nameInputIsValid && <p className='error-text'>Name must not be empty!</p>}
       </div>
       <div className='form-actions'>
