5c3e58092ef6a8566df015f4d0be8dde45fcfc6c
Jesse Abarca <JesseAbarca888@gmail.com>
2023-05-03 18:33:59 -0700

    Return isValid key and value in use-input

--- module-16-project/src/hooks/use-input.js
+++ module-16-project/src/hooks/use-input.js
@@ -17,6 +17,7 @@ const useInput = (validateValue) => {
 
     return {
         value: enteredValue,
+        isValid, valueIsValid,
         hasError,
         valueChangeHandler,
         inputBlurHandler
