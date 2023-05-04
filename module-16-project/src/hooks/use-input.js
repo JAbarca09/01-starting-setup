38b2ea42b42a4a88ef1592bda6214fabcbc83cd8
Jesse Abarca <JesseAbarca888@gmail.com>
2023-05-03 18:40:00 -0700

    Add reset function to use-Input hook

--- module-16-project/src/hooks/use-input.js
+++ module-16-project/src/hooks/use-input.js
@@ -1,27 +1,33 @@
 import { useState } from 'react';
 
 const useInput = (validateValue) => {
-    const [enteredValue, setEnteredValue] = useState('');
-    const [isTouched, setIsTouched] = useState(false);
+  const [enteredValue, setEnteredValue] = useState('');
+  const [isTouched, setIsTouched] = useState(false);
 
-    const valueIsValid = validateValue(enteredValue);
-    const hasError = !valueIsValid && isTouched;
+  const valueIsValid = validateValue(enteredValue);
+  const hasError = !valueIsValid && isTouched;
 
-    const valueChangeHandler = event => {
-        setEnteredValue(event.target.value);
-    };
+  const valueChangeHandler = (event) => {
+    setEnteredValue(event.target.value);
+  };
 
-    const inputBlurHandler = event => {
-        setIsTouched(true);
-    };
+  const inputBlurHandler = (event) => {
+    setIsTouched(true);
+  };
 
-    return {
-        value: enteredValue,
-        isValid, valueIsValid,
-        hasError,
-        valueChangeHandler,
-        inputBlurHandler
-    };
+  const reset = () => {
+    setEnteredValue('');
+    setIsTouched(false);
+  };
+
+  return {
+    value: enteredValue,
+    isValid: valueIsValid,
+    hasError,
+    valueChangeHandler,
+    inputBlurHandler,
+    reset,
+  };
 };
 
 export default useInput;
